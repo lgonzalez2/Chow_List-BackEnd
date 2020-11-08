a01989e8d6fe3f93b1e0b28c97817d4458524058
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-11-08 17:40:35 -0600

    tweak to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_cookie", domain: "http://localhost:3000"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_cookie", domain: "https://chow-list.herokuapp.com/"
 else 
     Rails.application.config.session_store :cookie_store, key: "_authentication_cookie"
 end 
