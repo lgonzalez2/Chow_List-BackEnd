5d4c7d665a5f1acd2c4c9d3708da2292a2ffa723
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-11-08 17:30:31 -0600

    added domain for production build

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,12 +15,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
   # for other domain origins******
-  # allow do
-  #   origins 'http://localhost:3001'
+  allow do
+    origins 'https://chow-list.herokuapp.com/'
 
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
