bbce40b3e6fb3e6e4cd7a2bc9850ed9278f8ff47
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-11-11 13:55:50 -0600

    configure

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origin 'http://localhost:3000'
 
     resource '*',
       headers: :any,
@@ -16,7 +16,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   # for other domain origins******
   allow do
-    origins 'https://chow-list.herokuapp.com/'
+    origin 'https://chow-list.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origin 'https://eloquent-euclid-ec101f.netlify.app/'
 
     resource '*',
       headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_cookie", domain: "https://chow-list.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_cookie", domain: "https://chow-list.herokuapp.com"
 else 
     Rails.application.config.session_store :cookie_store, key: "_authentication_cookie"
 end 
